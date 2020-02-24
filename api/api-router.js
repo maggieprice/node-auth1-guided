fabd02a83b0213f5c963aeddebb1e9503ae9ceb5
Justin Price <mr.price@Justins-iMac.local>
2020-02-24 13:17:01 -0500

    installed bcrypt

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -6,6 +7,16 @@ const usersRouter = require('../users/users-router.js');
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 
+
+router.get('/hash', (req, res) => {
+
+  const credentials = req.body;
+
+  const hash = bcrypt.hashSync(credentials.password, 14);
+
+credentials.password = hash;
+  res.json({ originalValue: authentication , hashedValue: hash })
+})
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
